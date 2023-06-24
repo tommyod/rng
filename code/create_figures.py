4f677961a1aaddaf2c1f32b2f9994439a8937bcf
tommyod <tommy.odland@gmail.com>
2023-06-24 16:38:44 +0200

    figures for mutual fund

--- code/create_figures.py
+++ code/create_figures.py
@@ -14,58 +14,139 @@
 # ---
 
 # %%
+import matplotlib as mpl
+
+# update matplotlibrc
+mpl.rcParams["font.family"] = "Open Sans"
+
+# %%
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import scipy as sp
+from scipy import stats
+
+# %matplotlib inline
+
+# %%
+import random
+
+random.gauss(0, 1)
 
 # %%
 SAVE_DIR = os.path.join("..", "presentation", "figures")
 
 # %% [markdown]
+# # General figures
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+# Plot PDF
+distribution = stats.uniform()
+x = np.linspace(-0.05, 1.05, num=2**10)
+ax.plot(x, distribution.pdf(x))
+ax.set_ylim([0, 1.2])
+
+fig.tight_layout()
+fig.savefig(os.path.join(SAVE_DIR, "uniform.pdf"))
+
+# Draw samples and plot them
+samples = distribution.rvs(1000, random_state=4)
+
+ax.scatter(
+    samples,
+    np.ones_like(samples) * stats.uniform().rvs(len(samples), random_state=1) * 0.2,
+    color="black",
+    s=1,
+)
+
+fig.savefig(os.path.join(SAVE_DIR, "uniform_samples.pdf"))
+
+ax.hist(samples, bins="auto", density=True, zorder=0)
+
+fig.savefig(os.path.join(SAVE_DIR, "uniform_samples_hist.pdf"))
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+# Plot PDF
+distribution = stats.norm()
+x = np.linspace(-4, 4, num=2**10)
+ax.plot(x, distribution.pdf(x))
+ax.set_ylim([0, 0.45])
+
+fig.tight_layout()
+fig.savefig(os.path.join(SAVE_DIR, "normal.pdf"))
+
+# Draw samples and plot them
+samples = distribution.rvs(1000, random_state=6)
+
+ax.scatter(
+    samples,
+    np.ones_like(samples) * stats.uniform().rvs(len(samples), random_state=1) * 0.1,
+    color="black",
+    s=1,
+)
+
+fig.savefig(os.path.join(SAVE_DIR, "normal_samples.pdf"))
+
+ax.hist(samples, bins="auto", density=True, zorder=0)
+
+fig.savefig(os.path.join(SAVE_DIR, "normal_samples_hist.pdf"))
+
+
+# %% [markdown]
 # # Mutual fund saving
 
 # %%
+def simulate(years, yearly, interest):
+    yield (saved := 0)
+    for year in range(years):
+        saved = saved * interest + yearly
+        yield saved
+
+
 years = 18
-initial = 0
 yearly = 12
 interest = 1.05
 
-def simulate(years, initial, yearly, interest):
-    saved = initial
-    for year in range(years):
-        saved = saved * interest
-        saved = saved + yearly
-        yield saved
-
-list(simulate(years, initial, yearly, interest))
+list(simulate(years, yearly, interest))
 
 # %%
 import random
 
+
+def simulate_rng(years, yearly, interest):
+    yield (saved := 0)
+    for year in range(years):
+        saved = saved * random.gauss(*interest) + yearly
+        yield saved
+
+
 years = 18
-initial = 0
 yearly = 12
 interest = (1.05, 0.1)
 
-def simulate(years, initial, yearly, interest):
-    saved = initial
-    for year in range(years):
-        saved = saved * random.gauss(*interest)
-        saved = saved + yearly
-        yield saved
-
-list(simulate(years, initial, yearly, interest))
-
 # %%
-for simulation in range(999):
-    
-    plt.plot(list(simulate(years, initial, yearly, interest)), color="black", alpha=0.01)
-    
-plt.tight_layout()
-plt.savefig(os.path.join(SAVE_DIR, "mutual_fund.pdf"))
-plt.show()
+fig, ax = plt.subplots(1, 1, figsize=(3, 9 / 4))
 
-# %%
+ax.plot(list(simulate(years, yearly, interest[0])), zorder=99, lw=3)
+ax.set_xticks(np.arange(0, years + 1, 2))
+ax.grid(True, ls="--", alpha=0.5, zorder=0)
+ax.set_xlabel("Years")
+ax.set_ylabel("Money")
+ax.set_ylim([0, 500])
+ax.set_xlim([0, 18])
 
-# %%
+fig.tight_layout()
+fig.savefig(os.path.join(SAVE_DIR, "mutual_fund.pdf"))
+
+for simulation in range(99):
+    ax.plot(
+        list(simulate_rng(years, yearly, interest)), color="black", alpha=0.1, zorder=9
+    )
+
+fig.savefig(os.path.join(SAVE_DIR, "mutual_fund_simulations.pdf"))
 
 # %%
